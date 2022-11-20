7b7c4fc1530e0084204d0e2593b315675f0fe9d1
6210110739@psu.ac.th <6210110739@psu.ac.th>
2022-11-20 23:50:45 +0700

    first commit

---
+++ server.php
@@ -0,0 +1,15 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "register_db";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if(!$conn) {
+        die("connection failed".mysqli_connect_error());    
+    }
+?>
